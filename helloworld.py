9cf1cd6b72ccbec0aee1b7cc791f4638fb466bfb
Hardik Prajapati <hardikprajapati32@gmail.com>
2018-11-18 14:51:17 +0530

    First step

---
+++ helloworld.py
@@ -0,0 +1,8 @@
+a = 10
+b = 10
+if a > b:
+    print("a is greater than b")
+elif b > a:
+    print("b is greater than a")
+else:
+    print("a is equal to b")
---
+++ whileloop.py
@@ -0,0 +1,17 @@
+maxIteration = int(input("Enter a number  :"))
+iteration = 0
+while(iteration < maxIteration):
+    iteration = iteration + 1
+    if iteration == 1:
+        print("Hello world for the ", iteration , "st time")
+    elif iteration == 2:
+        print("Hello world for the " , iteration , "nd time")
+    elif iteration == 3:
+        print("Hello world for the " , iteration , "rd time")
+    else:
+        print("Hello world for the " , iteration , "th time")
+
+while (maxIteration):
+    print ('Given flag is really true!')
+    maxIteration = 0
+print ("Good bye!")
