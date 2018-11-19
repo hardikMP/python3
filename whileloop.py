317bb0ebed35fb85fd8cee75925beeb297d3ed51
Hardik Prajapati <hardikprajapati32@gmail.com>
2018-11-19 15:55:26 +0530

    File deleted

--- whileloop.py
+++ whileloop.py
@@ -3,13 +3,13 @@ iteration = 0
 while(iteration < maxIteration):
     iteration = iteration + 1
     if iteration == 1:
-        print("Hello world for the ", iteration , "st time")
+        print("Hello world for the", iteration , "st time")
     elif iteration == 2:
-        print("Hello world for the " , iteration , "nd time")
+        print("Hello world for the", iteration , "nd time")
     elif iteration == 3:
-        print("Hello world for the " , iteration , "rd time")
+        print("Hello world for the", iteration , "rd time")
     else:
-        print("Hello world for the " , iteration , "th time")
+        print("Hello world for the", iteration , "th time")
 
 while (maxIteration):
     print ('Given flag is really true!')
